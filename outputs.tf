d2abad14b1ccf74e09ed4953cc5aa37125782fa8
danfedick <daniel.fedick@hashicorp.com>
2021-12-16 16:55:35 -0500

    Adding location to output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 
-/*
-output "rg_name" {
-  value = azurerm_resource_group.rg.name
+output "location" {
+  value = azurerm_resource_group.resource_group.location
+  description = "Resource Group Location"
 }
-*/
