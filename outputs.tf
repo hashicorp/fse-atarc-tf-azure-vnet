74e474e65cbdd0f831ace173b7896e710c75661a
danfedick <daniel.fedick@hashicorp.com>
2021-12-16 13:34:21 -0500

    changing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "rg_name" {
-  value = azurerm_resource_group.rg_name
+  value = azurerm_resource_group.rg.name
 }
 
 output "rg_id" {
-  value = azurerm_resource_group.rg_id
+  value = azurerm_resource_group.id
 }
