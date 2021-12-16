c607ebc679a07448d068d14facd4ac54a27e6fa7
danfedick <daniel.fedick@hashicorp.com>
2021-12-16 12:36:42 -0500

    Update name variables

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.ztarg.name
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.1.0.0/16"]
   subnet_prefixes     = ["10.1.1.0/24", "10.1.2.0/24"]
   subnet_names        = ["${var.name}-azure-subnet-a", "${var.name}-azure-subnet-b"]
